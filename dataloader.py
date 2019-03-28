b1f980e5342cf26c1ddddc75f09342d5217be123
hyzkent <zhuam@cse.ust.hk>
2019-03-28 20:32:07 +0800

    Add data augementation

--- dataloader.py
+++ dataloader.py
@@ -5,15 +5,6 @@ from torch.utils.data import DataLoader
 
 
 def get_data_loader(root, batch_size, split='train', num_workers=4):
-    # TODO: implement the data augmentation.
-    preprocess = transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.Resize((960, 720)),
-        transforms.RandomCrop((800, 600)),
-        # transforms.RandomRotation((-20, 20)),
-        transforms.ToTensor(),
-        # transforms.Normalize(mean=, std=)
-    ])
-    dset = OutdoorDataset(root=root, split=split, transform=preprocess)
+    dset = OutdoorDataset(root=root, split=split)
     loader = DataLoader(dset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     return loader
--- dataset.py
+++ dataset.py
@@ -1,17 +1,18 @@
 import torch
 import torch.utils.data as data
+import torchvision.transforms.functional as TF
+import torchvision.transforms as transforms
 from PIL import Image
 import os
 import numpy as np
+import random
 
 
 class OutdoorDataset(data.Dataset):
-    def __init__(self, root, split='train', transform=None, target_transform=None):
+    def __init__(self, root, split='train'):
         self.dataset = 'OutdoorDataset'
         self.root = root  # 把解压后的train文件夹放在dataset目录下
         self.split = split
-        self.transform = transform
-        self.target_transform = target_transform
         self.datapath = []  # every element contains path for image & path for mask
         with open(os.path.join(self.root, split), 'r') as f:
             ids = f.readlines()
@@ -24,6 +25,34 @@ class OutdoorDataset(data.Dataset):
                 }
             )
 
+    def transform(self, image, mask):
+        # Resize
+        resize_img = transforms.Resize(size=(960, 720), interpolation=Image.BILINEAR)
+        resize_mask = transforms.Resize(size=(960, 720), interpolation=Image.NEAREST)
+        image = resize_img(image)
+        mask = resize_mask(mask)
+
+        # Random Crop
+        i, j, h, w = transforms.RandomCrop.get_params(image, output_size=(800, 600))
+        image = TF.crop(image, i, j, h, w)
+        mask = TF.crop(mask, i, j, h, w)
+
+        # Random horizontal flipping
+        if random.random() > 0.5:
+            image = TF.hflip(image)
+            mask = TF.hflip(mask)
+
+        # Random vertical flipping
+        if random.random() > 0.5:
+            image = TF.vflip(image)
+            mask = TF.vflip(mask)
+
+        # Transform to tensor
+        image = TF.to_tensor(image)
+        mask = TF.to_tensor(mask)
+
+        return image, mask
+
     def __getitem__(self, index):
         img_path = self.datapath[index]['img']
         img = Image.open(img_path)
@@ -32,11 +61,7 @@ class OutdoorDataset(data.Dataset):
         mask = Image.open(mask_path)
         mask = np.array(mask, dtype=np.int8)
 
-        if self.transform is not None:
-            img = self.transform(img)
-
-        if self.target_transform is not None:
-            mask = self.target_transform(mask)
+        img, mask = self.transform(img, mask)
 
         return img, mask
 
