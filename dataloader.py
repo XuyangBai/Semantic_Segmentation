00017aba4fdfc0e70df42fd7817224aee77f7f35
hyzkent <zhuam@cse.ust.hk>
2019-03-28 19:53:14 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -7,11 +7,12 @@ from torch.utils.data import DataLoader
 def get_data_loader(root, batch_size, split='train', num_workers=4):
     # TODO: implement the data augmentation.
     preprocess = transforms.Compose([
-
+        transforms.RandomHorizontalFlip(),
+        transforms.Resize((960, 720)),
+        transforms.RandomCrop((800, 600)),
+        # transforms.RandomRotation((-20, 20)),
         transforms.ToTensor(),
         # transforms.Normalize(mean=, std=)
-        mytfm.RandomFlip2d((1, 1)),
-        mytfm.RandomCrop2d((256, 256)),
     ])
     dset = OutdoorDataset(root=root, split=split, transform=preprocess)
     loader = DataLoader(dset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
