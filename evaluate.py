1450eebe230def12441aacbc9036e59e8894f0c0
Xuyangbai <653823597@qq.com>
2019-03-29 20:13:21 +0800

    update eval

--- evaluate.py
+++ evaluate.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from utils import AverageMeter, accuracy, intersectionAndUnion
-
+from eval import get_confusion_matrix
 
 def cross_entropy2d(output, truth, weight=None, size_average=True):
     # input: (n, c, h, w), target: (n, h, w)
@@ -53,14 +53,33 @@ def evaluate(model, loader, gpu_mode, num_class=7):
         acc = accuracy(output, mask)
         acc_meter.update(acc)
 
+        if gpu_mode:
+            output = output.int().cpu().detach().numpy()
+            mask = mask.int().cpu().detach().numpy()
+        seg_pred = np.array(output)
+        seg_gt = np.array(mask)
+        ignore_index = seg_gt != 255
+        seg_gt = seg_gt[ignore_index]
+        seg_pred = seg_pred[ignore_index]
+        confusion_matrix += get_confusion_matrix(seg_gt, seg_pred, 7)
+
+        pos = confusion_matrix.sum(1)
+        res = confusion_matrix.sum(0)
+        tp = np.diag(confusion_matrix)
+
+        IU_array = (tp / np.maximum(1.0, pos + res - tp))
+        mean_IU = IU_array.mean()
+        
         # calculate iou
-        intersection, union = intersectionAndUnion(output, mask, num_class)
-        inter_meter.update(intersection)
-        union_meter.update(union)
+        # intersection, union = intersectionAndUnion(output, mask, num_class)
+        # inter_meter.update(intersection)
+        # union_meter.update(union)
 
     # summary
-    iou = inter_meter.sum / (union_meter.sum + 1e-10)
-    iou_mean = iou.mean()
+    iou = IU_array
+    iou_mean = IU_array.mean()
+    # iou = inter_meter.sum / (union_meter.sum + 1e-10)
+    # iou_mean = iou.mean()
     acc_mean = acc_meter.average()
     loss_mean = loss_meter.average()
 
